41b1ae2c4595eb96dbfd288d90a52f82c08d72ae
Josh Harris <me@celant.co.uk>
2024-07-11 12:10:16 +0100

    Allow pre-specified hub_id in turtles

--- turtle.lua
+++ turtle.lua
@@ -8,6 +8,16 @@ for _, filename in pairs(fs.list('/disk/turtle_files')) do
     fs.copy('/disk/turtle_files/' .. filename, '/' .. filename)
 end
 
+if fs.exists('/hub_id') then
+    file = fs.open('/hub_id', 'r')
+    hub_id = tonumber(file.readAll())
+    file.close()
+    if hub_id then
+        sleep(1)
+        os.reboot()
+    end
+end
+
 print("Enter ID of Hub computer to link to: ")
 hub_id = tonumber(read())
 if hub_id == nil then
